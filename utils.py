861ea0e9b98d94c9ba113169289a07a1dd923c01
VEXERAOP <98903684+VEXERAOP@users.noreply.github.com>
2022-02-24 01:17:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ from asyncio import sleep
 from pyrogram import Client
 from signal import SIGINT
 from random import randint
-from pytgcalls import GroupCallFactory
+import pytgcalls
 from pyrogram.errors import FloodWait
 from pyrogram.utils import MAX_CHANNEL_ID
 from pyrogram.raw.types import InputGroupCall
